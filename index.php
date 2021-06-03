350bfec3ba4832e55c26735407010f6fb3f7313e
Quangtoai <toai@gmail.com>
2021-06-04 01:04:25 +0700

    file fortend

---
+++ index.php
@@ -0,0 +1,68 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Trang web 2</title>
+    <link rel="stylesheet" type="text/css" href="./css/trangweb2.css"/>
+</head>
+<body>
+<div id="menu_top">
+    <ul>
+        <li><a href="#" title="Trang chủ" class="actit">Trang chủ</a></li>
+        <li><a href="#" title="Giới thiệu">Giới thiệu</a></li>
+        <li><a href="#" title="Tin tức">Tin tức</a></li>
+        <li><a href="#" title="Sản Phẩm">Sản phẩm</a></li>
+        <li><a href="#" title="Khuyến mãi">Khuyến mãi</a></li>
+        <li><a href="#" title="Dịch vụ">Dịch vụ</a></li>
+        <li><a href="#" title="Liên hệ">Liên hệ</a></li>
+    </ul>
+</div>
+<div id="banner"><img src="./public/img/Banner.png"/></div>
+<div id="main">
+    <div id="main_left">
+        <ul>
+            <li><img src="./public/img/hinhanh.jpg"/>
+                <h2><a href="#">Cựu ngôi sao Man Utd đổ gục xuống sân vì máy trợ tim hỏng</a></h2>
+                (Dân trí) - Daley Blind bất ngờ đổ gục xuống sân trong sự bàng hoàng của nhiều người khi máy trợ tim của anh bị hỏng Cựu ngôi sao Man Utd đổ gục xuống sân vì máy trợ tim
+            </li>
+            <li><img src="./public/img/hinhanh.jpg"/>
+                <h2><a href="#">Cựu ngôi sao Man Utd đổ gục xuống sân vì máy trợ tim hỏng</a></h2>
+                (Dân trí) - Daley Blind bất ngờ đổ gục xuống sân trong sự bàng hoàng của nhiều người khi máy trợ tim của anh bị hỏng Cựu ngôi sao Man Utd đổ gục xuống sân vì máy trợ tim
+            </li>
+
+            <li><img src="./public/img/hinhanh.jpg"/>
+                <h2><a href="#">Cựu ngôi sao Man Utd đổ gục xuống sân vì máy trợ tim hỏng</a></h2>
+                (Dân trí) - Daley Blind bất ngờ đổ gục xuống sân trong sự bàng hoàng của nhiều người khi máy trợ tim của anh bị hỏng Cựu ngôi sao Man Utd đổ gục xuống sân vì máy trợ tim
+            </li>
+            <li><img src="./public/img/hinhanh.jpg"/>
+                <h2><a href="#">Cựu ngôi sao Man Utd đổ gục xuống sân vì máy trợ tim hỏng</a></h2>
+                (Dân trí) - Daley Blind bất ngờ đổ gục xuống sân trong sự bàng hoàng của nhiều người khi máy trợ tim của anh bị hỏng Cựu ngôi sao Man Utd đổ gục xuống sân vì máy trợ tim
+            </li> <li><img src="./public/img/hinhanh.jpg"/>
+                <h2><a href="#">Cựu ngôi sao Man Utd đổ gục xuống sân vì máy trợ tim hỏng</a></h2>
+                (Dân trí) - Daley Blind bất ngờ đổ gục xuống sân trong sự bàng hoàng của nhiều người khi máy trợ tim của anh bị hỏng Cựu ngôi sao Man Utd đổ gục xuống sân vì máy trợ tim
+            </li>
+
+            <li><img src="./public/img/hinhanh.jpg"/>
+                <h2><a href="#">Cựu ngôi sao Man Utd đổ gục xuống sân vì máy trợ tim hỏng</a></h2>
+                (Dân trí) - Daley Blind bất ngờ đổ gục xuống sân trong sự bàng hoàng của nhiều người khi máy trợ tim của anh bị hỏng Cựu ngôi sao Man Utd đổ gục xuống sân vì máy trợ tim
+            </li>
+        </ul>
+    </div>
+    <div id="main_right">
+        <div id="back">Danh mục tin tức</div>
+        <ul>
+            <li><a href="#">Thực phẩm</a></li>
+            <li><a href="#">Đồ dùng</a></li>
+            <li><a href="#">Góc nhượng đồ</a></li>
+            <li><a href="#">Thế giới</a></li>
+            <li><a href="#">Trong nước</a></li>
+            <li><a href="#">Quốc phòng</a></li>
+            <li><a href="#">An ninh thủ đô</a></li>
+    </div>
+    <div id="div1"><a href="#"><img src="./public/img/banner1.jpg"/></a></div>
+</div>
+<div id="bot"> <h5>Mọi thông tin chi tiết xin vui lòng liên hệ SĐT: 0913341522</h5>
+
+</div>
+
+</body>
+</html>
